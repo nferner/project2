f4af7b1b4c33ace4cd3c7b16dbb29aaa1411a1a7
Neil Ferner <n.e.ferner@gmail.com>
2016-09-22 14:44:43 -0400

    add routes file to direct app's URL and setup history functionality

---
+++ src/config/routes.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Route, Router, hashHistory, IndexRoute } from 'react-router';
+import Main from '../../components/main.jsx';
+
+const Routes = () => {
+  return (
+    <Router history={hashHistory}>
+      <Route path="/" component={Main}>
+        
+      </Route>
+    </Router>
+  );
+}
