297eabfb686f0710179fffa13814fdf2466d5cfa
Neil Ferner <n.e.ferner@gmail.com>
2016-09-22 15:16:09 -0400

    add playlist file and connect

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Playlist from './playlist.jsx';
+// going to need to add
 class Main extends React.Component {
   render() {
     return (
       <div>
         <h1>Testing</h1>
+        <Playlist />
       </div>
     );
   }
--- src/components/playlist.jsx
+++ src/components/playlist.jsx
@@ -1,1 +1,13 @@
-// playlist functionality
+import React, { Component } from 'react';
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Name of Playlist</h3>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
