d1e1972dc73a8c5f51e8c52e48f45b6aa1624909
Neil Ferner <n.e.ferner@gmail.com>
2016-09-23 11:45:46 -0400

    pseudo code login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,45 @@
 // import react and components
+// import react router specifically withRouter
 // import firebase from firbase configuration
 
 // create a class for login and grab react
 // grab constructor from file
 // grab super from react
-// set state as an empty object 'string'
-//
+// set state as
+// set user name as empty string
+// set password as empty string
+
+// bind future use of handlechange
+// bind future use of handleSubmit
+
+// create handleChange of username and password function
+// create a stateObject that empty
+// create a stateKey that will grab the name from the event
+// set stateObject with the stateKey equal to the value of the target
+// pass the new stateObject through set the state of the login
+
+// create handleSubmit function
+// set variable of username and password equal to the newly changed state
+// call firebase supported by authorization
+// use firebases method of signInWithEmailAndPassword and pass through the inputted pword and user
+// use catch method if there is an error, pass the error through
+// set variable of errorCode equal to the err.code
+// set variable errorMessage equal to err.message;
+// console log that err
+// then push the new propertis through the router method to the dashboard file
+
+// render all this new information
+// return it
+// create div
+// create div with id for styling
+// create div hold/separate inputs
+// create input for username and onChange utilize handleChange
+// close div
+// create div
+// create input for pword and onchange utilize handleChange
+// close div
+// create button and on click use submit finction
+// close div
+// close div
+
 // export login
