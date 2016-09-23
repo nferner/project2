c7b25169948648f29ad06eac3fb9ab4956fd5b7f
Neil Ferner <n.e.ferner@gmail.com>
2016-09-23 11:21:06 -0400

    pseudo coding login

---
+++ src/components/login.jsx
@@ -0,0 +1,9 @@
+// import react and components
+// import firebase from firbase configuration
+
+// create a class for login and grab react
+// grab constructor from file
+// grab super from react
+// set state as an empty object 'string'
+//
+// export login
