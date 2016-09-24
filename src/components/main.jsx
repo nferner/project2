0a88c02f1a1e074491e8f5aee5fd19790bd39bc3
Neil Ferner <n.e.ferner@gmail.com>
2016-09-24 15:23:48 -0400

    add pseudo code for get request

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-// going to need to add
+// add superagent for get requests
+
 const propTypes = {
   children: React.PropTypes.element,
 };
 
 class Main extends Component {
+// add constructor to pass props
+// add super and pass props
+// this.state has the variable playlist equal to an empty object
+// add component did mount to retrieve the playlist when page is opened
+// function for getting playlist goes here
+
+// add 'get' request
+// set the url as a variable 'const' call url or base url
+/* need to use the 'request' from superagent to 'get' information from the url. pass the url
+    through 'get'*/
+/* use .then with 'response' from database passed through, fat arrow function cause its supposed to
+    do something*/
+/* like get the playlist set playlist as variable and set it equal to the response use the
+    .body method*/
+// now set the state equal to the palylist variable
+
+// need ability to 'post' information
+
+// need ability to edit 'push' information
+
+// need ability to delete 'del' information
+
+
   render() {
     return (
       <div>
